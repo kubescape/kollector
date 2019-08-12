ff51fb2514df03a686f5c7b781540eeefff83ae6
Bezalel Brandwine <bbrandwine@cyberarmor.io>
2019-08-12 09:14:37 -0400

    comment out failed tests

--- watch/idmanagment_test.go
+++ watch/idmanagment_test.go
@@ -1,22 +1,21 @@
 package watch
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestIdsManagment1(test *testing.T) {
-	s := CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	test.Errorf("123")
+	// s := CreateID()
+	// fmt.Printf("%d\n", s)
+	// s = CreateID()
+	// fmt.Printf("%d\n", s)
+	// s = CreateID()
+	// fmt.Printf("%d\n", s)
+	// s = CreateID()
+	// fmt.Printf("%d\n", s)
+	// s = CreateID()
+	// fmt.Printf("%d\n", s)
+	// s = CreateID()
+	// fmt.Printf("%d\n", s)
+	// test.Errorf("123")
 }
