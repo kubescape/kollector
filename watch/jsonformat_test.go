62179d8d0cdb9231000f962b72eefd253d0a0091
Raziel Cohen <rcohen@cyberarmor.io>
2019-08-14 13:35:02 +0300

    make tests passed

--- watch/jsonformat_test.go
+++ watch/jsonformat_test.go
@@ -1,7 +1,6 @@
 package watch
 
 import (
-<<<<<<< HEAD
 	"bytes"
 	"fmt"
 	"testing"
@@ -26,29 +25,4 @@ func TestJson(test *testing.T) {
 	if bytes.Compare(wh.jsonReport.Pods.Updated[0].([]byte), []byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs")) != 0 {
 		test.Errorf("PODS")
 	}
-=======
-	"testing"
-)
-
-// func TestJson(test *testing.T) {
-// 	AddToJsonFormat([]byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs"), NODE, CREATED)
-// 	AddToJsonFormat([]byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs"), SERVICES, DELETED)
-// 	AddToJsonFormat([]byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs"), PODS, UPDATED)
-
-// 	fmt.Printf("json %s\n", string(PrepareDataToSend()))
-// 	test.Errorf("123")
-// }
-
-type abc struct {
-	A int `json:"a"`
-}
-
-func TestJson1(test *testing.T) {
-	// var q abc = abc{A: 1}
-
-	// fmt.Printf("123456 %d\n", q.A)
-	// w, _ := json.Marshal(q)
-	// fmt.Printf("123456 %s\n", string(w))
-	// test.Errorf("123")
->>>>>>> 4585080cdfe5a3fd4568c5d02ff0ca7782f593a6
 }
