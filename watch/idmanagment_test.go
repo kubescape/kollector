6b8565ae2655330a06a2d94faf470474a7a5e9af
Raziel Cohen <rcohen@cyberarmor.io>
2019-08-15 12:15:52 +0300

    add tests

--- watch/idmanagment_test.go
+++ watch/idmanagment_test.go
@@ -6,17 +6,21 @@ import (
 )
 
 func TestIdsManagment1(test *testing.T) {
-	s := CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	s = CreateID()
-	fmt.Printf("%d\n", s)
-	//test.Errorf("123")
+	s0 := CreateID()
+	fmt.Printf("%d\n", s0)
+	s1 := CreateID()
+	fmt.Printf("%d\n", s1)
+	if s1 == s0 {
+		test.Errorf("ids equal s0 s1")
+	}
+
+	s2 := CreateID()
+	fmt.Printf("%d\n", s2)
+	if s1 == s2 {
+		test.Errorf("ids equal s2 s1")
+	}
+	if s2 == s0 {
+		test.Errorf("ids equal s0 s2")
+	}
+
 }
--- watch/jsonformat_test.go
+++ watch/jsonformat_test.go
@@ -1,1 +1,28 @@
 package watch
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+var (
+	wh WatchHandler
+)
+
+func TestJson(test *testing.T) {
+	wh.jsonReport.AddToJsonFormat([]byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs"), NODE, CREATED)
+	wh.jsonReport.AddToJsonFormat([]byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs"), SERVICES, DELETED)
+	wh.jsonReport.AddToJsonFormat([]byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs"), PODS, UPDATED)
+
+	fmt.Printf("json %s\n", string(PrepareDataToSend(&wh)))
+	if bytes.Compare(wh.jsonReport.Nodes.Created[0].([]byte), []byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs")) != 0 {
+		test.Errorf("NODE")
+	}
+	if bytes.Compare(wh.jsonReport.Services.Deleted[0].([]byte), []byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs")) != 0 {
+		test.Errorf("SERVICES")
+	}
+	if bytes.Compare(wh.jsonReport.Pods.Updated[0].([]byte), []byte("12343589thfgnvdfklbnvklbnmdfk'lbgfbhs")) != 0 {
+		test.Errorf("PODS")
+	}
+}
