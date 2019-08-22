795a8e6777bf3cc212312fcb198f5690562ed91c
Raziel Cohen <rcohen@cyberarmor.io>
2019-08-22 11:03:57 +0300

    add pong handler

--- watch/websocket.go
+++ watch/websocket.go
@@ -10,6 +10,8 @@ import (
 
 type ReqType int
 
+const MAXPINGMESSAGE = 3
+
 const (
 	PING    ReqType = 0
 	MESSAGE ReqType = 1
@@ -22,57 +24,70 @@ type DataSocket struct {
 }
 
 type WebSocketHandler struct {
-	data chan DataSocket
+	data             chan DataSocket
+	conn             *websocket.Conn
+	u                url.URL
+	keepAliveCounter int
 }
 
 func CreateWebSocketHandler() *WebSocketHandler {
-	return &WebSocketHandler{data: make(chan DataSocket)}
+	return &WebSocketHandler{data: make(chan DataSocket), keepAliveCounter: 0}
 }
 
+func (wsh *WebSocketHandler) reconnectToWebSocket() {
+	reconnectionCounter := 0
+	var err error
+	wsh.conn, _, err = websocket.DefaultDialer.Dial(wsh.u.String(), nil)
+
+	for err != nil {
+		log.Printf("dial: %v", err)
+		reconnectionCounter++
+		log.Printf("wait 5 seconds before tring to reconnect")
+		time.Sleep(time.Second * 5)
+		log.Printf("reconnect try number %d", reconnectionCounter+1)
+		wsh.conn, _, err = websocket.DefaultDialer.Dial(wsh.u.String(), nil)
+	}
+	wsh.conn.SetPongHandler(func(string) error {
+		log.Printf("in PongHandler")
+		wsh.keepAliveCounter = 0
+		return nil
+	})
+	go func() {
+		for {
+			wsh.conn.ReadMessage()
+		}
+	}()
+}
+
+//StartWebSokcetClient -
 func (wsh *WebSocketHandler) StartWebSokcetClient(urlWS string, path string, cluster_name string, customer_guid string) {
 
-	u := url.URL{Scheme: "wss", Host: urlWS, Path: path, ForceQuery: true}
-	q := u.Query()
+	wsh.u = url.URL{Scheme: "wss", Host: urlWS, Path: path, ForceQuery: true}
+	q := wsh.u.Query()
 	q.Add("customerGUID", customer_guid)
 	q.Add("clusterName", cluster_name)
-	u.RawQuery = q.Encode()
-	log.Printf("connecting to %s", u.String())
+	wsh.u.RawQuery = q.Encode()
+	log.Printf("connecting to %s", wsh.u.String())
+	wsh.reconnectToWebSocket()
 
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
 	//defer conn.Close()
 
-	go func(conn *websocket.Conn) {
+	go func() {
 		for {
 			data := <-wsh.data
 			switch data.RType {
 			case MESSAGE:
 				log.Println("Sending: message.")
-				err := conn.WriteMessage(websocket.TextMessage, []byte(data.message))
+				err := wsh.conn.WriteMessage(websocket.TextMessage, []byte(data.message))
 				if err != nil {
 					log.Println("WriteMessage to websocket:", err)
-					i := 0
-					for i < 3 {
-						log.Printf("reconnect try number %d of 3\n", i+1)
-						conn, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
-						if err != nil {
-							log.Fatal("dial:", err)
-						}
-						err := conn.WriteMessage(websocket.TextMessage, []byte(data.message))
-						if err != nil {
-							log.Println("WriteMessage to websocket:", err)
-						} else {
-							log.Println("resending: message.")
-							break
-						}
-						i++
-					}
-					if i < 3 {
-						continue
+					wsh.reconnectToWebSocket()
+					err := wsh.conn.WriteMessage(websocket.TextMessage, []byte(data.message))
+					if err != nil {
+						log.Println("WriteMessage to websocket:", err)
 					} else {
-						log.Printf("reconnection failed fo 3 times, exiting\n")
+						log.Println("resending: message.")
+						break
 					}
 				}
 			case EXIT:
@@ -80,49 +95,24 @@ func (wsh *WebSocketHandler) StartWebSokcetClient(urlWS string, path string, clu
 				return
 			}
 		}
-	}(conn)
+	}()
 
-	go func(conn *websocket.Conn) {
+	go func() {
 		for {
+			time.Sleep(40 * time.Second)
 			log.Println("Sending: ping.")
-			err = conn.WriteMessage(websocket.PingMessage, []byte{})
+			err := wsh.conn.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(5*time.Second))
 			if err != nil {
 				log.Println("Write Error: ", err)
-				break
 			}
+			wsh.keepAliveCounter++
 
-			msgType, _, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("WebSocket closed.")
-				data := DataSocket{message: "WebSocket closed", RType: EXIT}
-				wsh.data <- data
-				i := 0
-				for i < 3 {
-					log.Printf("reconnect try number %d of 3", i+1)
-					conn, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
-					if err != nil {
-						log.Fatal("dial:", err)
-					}
-					i++
-				}
-				if i < 3 {
-					continue
-				} else {
-					log.Printf("reconnection failed fo 3 times, exiting\n")
-				}
-				return
+			if wsh.keepAliveCounter == MAXPINGMESSAGE {
+				wsh.conn.Close()
+				wsh.reconnectToWebSocket()
 			}
-			// We don't recognize any message that is not "pong".
-			if msgType != websocket.PongMessage {
-				log.Println("Unrecognized message received.")
-				time.Sleep(40 * time.Second)
-				continue
-			} else {
-				log.Println("Received: pong.")
-			}
-			time.Sleep(40 * time.Second)
 		}
-	}(conn)
+	}()
 }
 
 //SendMessageToWebSocket -
