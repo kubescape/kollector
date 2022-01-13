32954e6d17791a603bf620b38c2b7b770121318f
Bezalel Brandwine <bbrandwine@armosec.io>
2022-01-13 10:49:04 +0200

    add process as default in docker enrtypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 EXIT_CODE=0
 
+EXEC_COMMAND_ARGS=$@
+
+if test -f "$1"; then
+    echo "$1 exists."
+else
+    echo "$1 not exists."
+    EXEC_COMMAND_ARGS="/k8s-ca-dashboard-aggregator "$@
+fi
+
 # exit code 2 means we have watch timeout just need to reconnect
 while true; do
-    $@
+    $EXEC_COMMAND_ARGS
     EXIT_CODE=$?    
-    echo "$@" " exited with code "$EXIT_CODE
+    echo "$EXEC_COMMAND_ARGS" " exited with code "$EXIT_CODE
 
     if [ $EXIT_CODE -ne 4 ]; then
         exit $EXIT_CODE
