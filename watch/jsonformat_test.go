b45f29412bea5a4deb384b846a18f7f47145a695
Daniel-GrunbergerCA <danielg@armosec.io>
2023-05-24 08:56:00 +0300

    test
    Signed-off-by: Daniel-GrunbergerCA <danielg@armosec.io>

--- watch/jsonformat_test.go
+++ watch/jsonformat_test.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	v1core "k8s.io/api/core/v1"
 )
 
 var (
@@ -36,5 +39,80 @@ func TestIsEmptyFirstReport(test *testing.T) {
 	jsonReportToSend, _ = json.Marshal(jsonReport)
 	if isEmptyFirstReport(jsonReportToSend) {
 		test.Errorf("First report is not empty")
+	}
+}
+
+func TestSetInstallationData(t *testing.T) {
+	testCases := []struct {
+		cfgMapData        map[string]string
+		expectedError     bool
+		expectedErrorMsg  string
+		expectedStorage   bool
+		expectedNodeAgent bool
+	}{
+		// Test case 1: Valid clusterData
+		{
+			cfgMapData: map[string]string{
+				"clusterData": `{"storage": "true", "nodeAgent": "false"}`,
+			},
+			expectedError:     false,
+			expectedStorage:   true,
+			expectedNodeAgent: false,
+		},
+		// Test case 2: Missing clusterData
+		{
+			cfgMapData:        map[string]string{},
+			expectedError:     true,
+			expectedStorage:   false,
+			expectedNodeAgent: false,
+		},
+		// Test case 3: Invalid clusterData JSON
+		{
+			cfgMapData: map[string]string{
+				"clusterData": `{"storage": "true", "nodeAgent": "invalid"}`,
+			},
+			expectedError:     false,
+			expectedStorage:   true,
+			expectedNodeAgent: false,
+		},
+		// Test case 4: Both storage and nodeAgent are false
+		{
+			cfgMapData: map[string]string{
+				"clusterData": `{"storage": "false", "nodeAgent": "false"}`,
+			},
+			expectedError:     false,
+			expectedStorage:   false,
+			expectedNodeAgent: false,
+		},
+		// Test case 5: Both storage and nodeAgent are true
+		{
+			cfgMapData: map[string]string{
+				"clusterData": `{"storage": "true", "nodeAgent": "true"}`,
+			},
+			expectedError:     false,
+			expectedStorage:   true,
+			expectedNodeAgent: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		cfgMap := &v1core.ConfigMap{
+			Data: tc.cfgMapData,
+		}
+
+		jsonReport := &jsonFormat{}
+		err := setInstallationData(cfgMap, jsonReport)
+		if tc.expectedError {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+
+			if jsonReport.InstallationData.StorageEnabled != tc.expectedStorage {
+				t.Errorf("expected StorageEnabled to be %v, got %v", tc.expectedStorage, jsonReport.InstallationData.StorageEnabled)
+			}
+			if jsonReport.InstallationData.NodeAgentEnabled != tc.expectedNodeAgent {
+				t.Errorf("expected NodeAgentEnabled to be %v, got %v", tc.expectedNodeAgent, jsonReport.InstallationData.NodeAgentEnabled)
+			}
+		}
 	}
 }
