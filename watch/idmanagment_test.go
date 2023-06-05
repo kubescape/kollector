162c0afecc096b7fe9189668562b89f7e935d1dc
Daniel-GrunbergerCA <danielg@armosec.io>
2023-06-05 10:51:26 +0300

    blank line

--- watch/idmanagment_test.go
+++ watch/idmanagment_test.go
@@ -13,6 +13,7 @@ func TestCreateID(t *testing.T) {
 	assert.NotEqual(t, s1, s0, "ids equal")
 
 	s2 := CreateID()
+
 	assert.NotEqual(t, s1, s2, "ids equal")
 	assert.NotEqual(t, s2, s0, "ids equal")
 }
